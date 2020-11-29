7cc6325e84baa62fe7a5ff9b16074ff6891110e8
tuancanhnb <midnight2342000@gmail.com>
2020-11-30 01:13:58 +0700

    continue

--- dong_ho.py
+++ dong_ho.py
@@ -8,16 +8,59 @@ pygame.init()
 screen = pygame.display.set_mode((500,600))
 
 # khai báo màu
-GREY = (150,150,150)
+GREEN = (58, 191, 55)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
 
 # khai báo biến
 running = True
+
+#khai báo tạo font chữ 
+font = pygame.font.SysFont('sans', 50)
+text_1 = font.render('+', True, RED)
+text_2 = font.render('+', True, RED)
+text_3 = font.render('Start', True, RED)
+text_4 = font.render('-', False, RED)
+text_5 = font.render('-', False, RED)
+text_6 = font.render('Reset', False, RED)
 
 # vòng lặp game (giúp hiển thị trên màn hình ko bị tắt)
 while running:
 
     # thay đổi màu nền của chương trình
-    screen.fill(GREY)
+    screen.fill(GREEN)
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
+    # vẽ lên 1 hình chữ nhật
+    pygame.draw.rect(screen, WHITE, (100,50,50,50))
+    pygame.draw.rect(screen, WHITE, (200,50,50,50))
+    pygame.draw.rect(screen, WHITE, (300,50,150,50))
+    pygame.draw.rect(screen, WHITE, (100,200,50,50))
+    pygame.draw.rect(screen, WHITE, (200,200,50,50))
+    pygame.draw.rect(screen, WHITE, (300,150,150,50))
+
+    # vẽ các kí tự vào các ô
+    screen.blit(text_1, (113,45))
+    screen.blit(text_2, (213,45))
+    screen.blit(text_3, (313,45))
+    screen.blit(text_4, (117,193))
+    screen.blit(text_5, (219,193))
+    screen.blit(text_6, (313,145))
+
+    # vẽ hình chữ nhật dài cuối
+    pygame.draw.rect(screen, BLACK, (50,520,400,50))
+    pygame.draw.rect(screen, WHITE, (60,530,380,30))
+
+    # vẽ hình tròn
+    pygame.draw.circle(screen, BLACK, (250,400),100)
+    pygame.draw.circle(screen, WHITE, (250,400),95)
+    pygame.draw.circle(screen, BLACK, (250,400),5)
+
+    # vẽ đường thẳng ở tâm hình tròn
+    pygame.draw.line(screen, BLACK, (250,400),(250,310))
+
 
     # sét các sự kiện như phím tắt, click chuột,...
     for event in pygame.event.get():
